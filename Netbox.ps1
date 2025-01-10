2eea6e5eecceff1075be45c0bf555ad43329f392
shb256 <shb256@gmail.com>
2025-01-10 15:10:18 +0100

    Update Netbox.ps1

--- Netbox.ps1
+++ Netbox.ps1
@@ -1,7 +1,7 @@
 ﻿param (
     [string]$ApiToken       # Definiert den Parameter "DeviceId" als string
 )
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install wireguard -y
 
 # Konfigurationsvariablen
