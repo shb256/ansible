97d99b97d49e778430ef21ba8bc288c258e95625
shb256 <shb256@gmail.com>
2025-01-10 15:03:23 +0100

    Update Netbox.ps1

--- Netbox.ps1
+++ Netbox.ps1
@@ -1,6 +1,9 @@
 ﻿param (
     [string]$ApiToken       # Definiert den Parameter "DeviceId" als string
 )
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install wireguard -y
+
 # Konfigurationsvariablen
 $NetboxUrl = "https://nb.durchhalten.org/api"  # URL zu deiner NetBox API
 #$ApiToken = ""            # Dein NetBox API-Token
